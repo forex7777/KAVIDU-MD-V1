0ec5cf3f64ba4b6cf067f6beb9f60207a001f594
forex7777 <iharasevinda3@gmail.com>
2025-07-31 19:41:11 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -5,16 +5,16 @@ cmd({
     pattern: "ping",
     desc: "Check bot's response time.",
     category: "main",
-    react: "🍂",
+    react: "🚀",
     filename: __filename
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         const startTime = Date.now()
-        const message = await conn.sendMessage(from, { text: '> *PINGING...*' })
+        const message = await conn.sendMessage(from, { text: '> *⚕️PINGING....*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `> *🔥 SPEED : ${ping}ms*` }, { quoted: message })
+        await conn.sendMessage(from, { text: `> *⚡ SPEED : ${ping}ms*` }, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
